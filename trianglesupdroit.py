a8f9ec370f6e4027c0f9a491e12f0ea0ccc61071
Fernand <fernand@gmail.com>
2024-06-19 18:45:44 +0200

    exercice sommes_valeurs_naturelles

--- trianglesupdroit.py
+++ trianglesupdroit.py
@@ -1,15 +1,3 @@
-"""
-n = int(input("entrez un nombre"))
-i = 0 
-
-while True:
-    for j in range(n):
-        print("X", sep ="", end = "")   
-    print("")
-    n -= 1
-    if n == 0 :
-        break
-"""
 n = int(input("entrez un nombre"))
 i = 0
 
@@ -22,3 +10,4 @@ while n > 0 :
     print("")
     n -= 1 
     i += 1
+
